9cbfc1632b2b5ba9c0759648a4d50d583eef2f1c
Lucas Combs <l-combs@hotmail.com>
2018-09-04 11:47:02 -0400

    Added comments to improve data validation

--- Lab2.rb
+++ Lab2.rb
@@ -11,26 +11,28 @@ DAYS_PER_WEEK = 7
 MINS_PER_WEEK = MINS_PER_HOUR * HOURS_PER_DAY * DAYS_PER_WEEK
 
 # Get data from user
-print "Enter the plan type (C-Commerical, R-Residential, S-Student): "
-planType = gets().chomp().upcase()
+	print "Enter the plan type (C-Commerical, R-Residential, S-Student): "
+	planType = gets().chomp().upcase()
 
-print "Enter the number of talk minutes used: "
-numTalkMinutes = gets().to_i()
+	print "Enter the number of talk minutes used: "
+	numTalkMinutes = gets().to_i()
 
 # Validate data
-dataValid = true # Assume data to be valid
+	parametersValid = true # Assume data to be valid
 
-# Check for valid number of talk minutes
-if (numTalkMinutes < 0 || numTalkMinutes > MINS_PER_WEEK)
-	dataValid = false;
-	puts "Talk minutes n must be: 0 >= n <= #{MINS_PER_WEEK.to_s()}. You entered: '#{numTalkMinutes}'"
-end;
+	# Check for valid number of talk minutes
+	if (numTalkMinutes < 0 || numTalkMinutes > MINS_PER_WEEK)
+		parametersValid = false;
+		puts "Talk minutes n must be: 0 >= n <= #{MINS_PER_WEEK.to_s()}. You entered: '#{numTalkMinutes}'"
+	end;
 
-if (case planType when "C","R","S" then false else true end)
-	dataValid = false;
-	puts "Valid plan types are: C-Commerical, R-Residential, S-Student. You entered: '#{planType}'."
-end;
+	# Check for valid plan type
+	if (case planType when "C","R","S" then false else true end)
+		parametersValid = false;
+		puts "Valid plan types are: C-Commerical, R-Residential, S-Student. You entered: '#{planType}'."
+	end;
 
-if (!dataValid)
-	return
-end;
+	# Exit if parameters are not valid
+	if (!parametersValid)
+		return
+	end;
