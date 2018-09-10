5ec15daae10a6631ae5d4a3856d8370f1795a713
Lucas Combs <l-combs@hotmail.com>
2018-09-10 17:11:03 -0400

    Fixed spelling error

--- Lab3_Part9.rb
+++ Lab3_Part9.rb
@@ -18,7 +18,7 @@ puts "n\tn^2\tn^3\tn^4"
 
 # Loop over each integer in range
 for n in 1...value+1 do
-	# Loop over possible exponenets
+	# Loop over possible exponents
 	for power in 1...5 do
 		print "#{n**power}\t"
 	end;
