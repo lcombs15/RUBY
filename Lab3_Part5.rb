15b1a7fa02d70ef55c9e15dc349f64efe8a0a2c0
Lucas Combs <l-combs@hotmail.com>
2018-09-10 16:25:52 -0400

    Comments & totals for odd and total input.

--- Lab3_Part5.rb
+++ Lab3_Part5.rb
@@ -3,12 +3,30 @@
 # Lab 3
 # Combs, Lucas
 
+# Initialize Variables
 sum = 0
+count = 0
+odd = 0
+
+# Prompt user for input
 print("Enter an integer value (0 to quit): ")
 value = gets().to_i()
+
 while (value != 0) do
-sum += value;
-print("Enter next value: ")
-value = gets().to_i()
+	# Record totals
+	sum += value;
+	count += 1;
+
+	# Count number of odd values
+	if (value % 2 == 1) then
+		odd += 1
+	end;
+	
+	# Prompt user again
+	print("Enter next value: ")
+	value = gets().to_i()
 end
+
+# Display Summary
+puts("# of values: #{count}")
 puts("The sum of the entered values is: #{sum}")
