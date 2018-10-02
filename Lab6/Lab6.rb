385ea571fabe09af254e6a0a6ad0bb54b12fd052
Lucas Combs <l-combs@hotmail.com>
2018-10-02 11:17:40 -0400

    Got and validated user data

--- Lab6/Lab6.rb
+++ Lab6/Lab6.rb
@@ -3,3 +3,12 @@
 # Lab 6
 # Combs, Lucas
 
+# Get Data from user
+print "Enter the number of seconds to sleep: "
+sleep_seconds = gets().to_i()
+
+# Validate data from user
+if sleep_seconds <= 0
+	puts "Valid entry is any number greater than zero. You Entered #{sleep_seconds}"
+	return
+end
