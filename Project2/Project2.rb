a2ed5818f4bbd9597a716e3c5b28f096d986ec78
Lucas Combs <l-combs@hotmail.com>
2018-10-06 18:23:11 -0400

    Completed Project 2

--- Project2/Project2.rb
+++ Project2/Project2.rb
@@ -18,13 +18,62 @@ if num_athletes <= 0
 	return
 end
 
-# Initialize dataset with default = 0
+# Initialize time dataset with default = 0
 time_data = Array.new(num_athletes) { Array.new(NUM_EVENTS, 0) }
 
-for athlete in time_data
-	for event in 0...athlete.length
-		print athlete[event] = rand(EVENT_MIN[event].to_i..EVENT_MAX[event].to_i)
-		print "\t"
+# Initialize time total datasets with default
+athlete_sum_data = Array.new(num_athletes, 0)
+event_sum_data = Array.new(NUM_EVENTS, 0)
+
+# Print report header
+puts	"\nTri-Athalon Time Results\n"\
+		"Athlete #\tSwim\t\t\tRun\t\t\tBike\t\t\tTotal Time\n"\
+		"---------\t----\t\t\t----\t\t\t----\t\t\t-----------\n"
+
+
+# Loop through all athletes
+for athlete_num in 0...time_data.length
+	# Store reference to current athlete
+	athlete_times = time_data[athlete_num]
+	
+	print "#{athlete_num + 1}\t\t"
+	
+	# Loop through events for given athlete
+	for event_id in 0...athlete_times.length
+		# Store randomly generated time within bounds
+		time = rand(EVENT_MIN[event_id].to_i..EVENT_MAX[event_id].to_i)
+		
+		# Record generated time for athlete
+		athlete_times[event_id] = time
+		
+		# Add time to athletes total
+		athlete_sum_data[athlete_num] += time
+		
+		# Add time to event total
+		event_sum_data[event_id] += time
+		
+		# Print out time
+		print "#{time / 60}:#{time % 60} (#{time})\t\t"
 	end
-	puts
+	
+	# Print out athlete total
+	total = athlete_sum_data[athlete_num]
+	print "#{total / 60}:#{total % 60} (#{total})\n"
 end
+
+# Display totals
+print "\nAvg times:\t"
+grand_total = 0
+
+# Loop through events
+for event in event_sum_data
+	# Keep track of total
+	grand_total += event
+	
+	# Calculare and display event average
+	avg = event / num_athletes
+	print "#{avg / 60}:#{avg % 60} (#{avg})\t\t"
+end
+
+# Display grand total for time
+print "#{grand_total / 60}:#{grand_total % 60} (#{grand_total})"
