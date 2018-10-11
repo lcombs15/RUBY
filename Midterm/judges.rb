1fbc3ddb7ab41316ec969bdf77079f2abbdcc069
Lucas Combs <l-combs@hotmail.com>
2018-10-11 11:38:31 -0400

    Midterm complete

--- Midterm/judges.rb
+++ Midterm/judges.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/ruby -w
 # CIT 383
-# Midterm
+# Midterm - 11 Oct 2018
 # Combs, Lucas
 
 # Define constants
 NUM_JUDGES = 5
 MIN_SCORE = 5
-MAX_SCORE = 5
+MAX_SCORE = 10
 MIN_PERFORMERS = 1
 MAX_PERFORMERS = 10
 
 # Get input for user
 num_performers = 0
 while (num_performers < MIN_PERFORMERS || num_performers > MAX_PERFORMERS) do
-	print "Please enter the number of performers (1-5): "
+	print "Please enter the number of performers (#{MIN_PERFORMERS}-#{MAX_PERFORMERS}): "
 	num_performers = gets().to_i()	
 end
 
@@ -36,8 +36,14 @@ end
 # Calculate averages
 average = Array.new(num_performers)
 for p in 0...num_performers
+	# Grab scores
 	player_scores = scores[p]
-	average[p] = (sums[p] - (player_scores.min() + player_scores.max())) / NUM_JUDGES - 2
+
+	# Calculate avg
+	average[p] = (sums[p] - (player_scores.min() + player_scores.max())) / (NUM_JUDGES - 2).to_f
+
+	# Round avg
+	average[p] = average[p].round(2)
 end
 
 # Print report heading
@@ -51,5 +57,20 @@ print "Avg\n"
 
 # Each column gets an underline
 (NUM_JUDGES + 2).times {print "-------\t\t"}
+
+# New line after underlines
 puts
 
+# Print scores by performer
+for p in 0...num_performers
+	# Print performer #
+	print "#{p + 1}\t\t"
+
+	# Print scores for judges
+	for j in 0...NUM_JUDGES
+		print "#{scores[p][j]}\t\t"
+	end
+
+	# Print average
+	print "#{average[p]}\n"
+end
