60cd2c791ff499e61e1c1033d4bc5f5eb58d263b
Lucas Combs <l-combs@hotmail.com>
2018-10-19 09:37:06 -0400

    Added small script to quickstart dev

---
+++ WORK.bat
@@ -0,0 +1,7 @@
+@echo off
+start cmd.exe /k "cd C:\Users\Lucas\Dropbox\CIT 383\RUBY"
+cd "C:\Program Files\Git\"
+start git-bash.exe
+cd "C:\Program Files (x86)\Notepad++\"
+start notepad++.exe
+exit
