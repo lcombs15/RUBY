ce7aaab23bcc7c124d4b3ac779c323a7665a0fc5
Lucas Combs <l-combs@hotmail.com>
2018-12-12 21:15:45 -0500

    Got CMD args working

--- Final Exam/FinalExam.rb
+++ Final Exam/FinalExam.rb
@@ -3,3 +3,34 @@
 # Final Exam
 # Combs, Lucas
 
+# Need to be able to use files
+require 'fileutils'
+
+# Need command line parsing
+require 'optparse'
+
+# Determine options from command line
+def parseCommandLine()
+
+	# Default to help command if not arguments given
+	if ARGV.empty? then
+		ARGV[0] = "--help"
+	end
+
+	OptionParser.new do |options|
+
+		options.banner = "USAGE: FinalExam.rb [options] count"
+
+	 	options.on("-c c", "--countlogs c", "Find users with at least C logins") do |c|
+	 		puts "Will find: #{c} logins."
+	 		return
+	 	end
+
+	 	options.on("-h", "--help", "Print this message") do
+	 		puts options
+	 		return
+	 	end
+	end.parse!
+end
+
+parseCommandLine()
